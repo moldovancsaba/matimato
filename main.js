8d4596b88239bc4ae16de3dfddf2b0799f4ea42e
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2023-12-08 22:32:49 +0100

    active row

--- main.js
+++ main.js
@@ -44,54 +44,87 @@ class Player {
 const board = new Board();
 const humanPlayer = new Player();
 const computerPlayer = new Player();
-let activeRow = 4; // A középső sor indexe (0-tól indexelve).
+let activeRow = 4; // Kezdés a középső sorral
+
+function createBoard(board) {
+    const boardElement = document.getElementById('board');
+    boardElement.innerHTML = '';
+    for (let i = 0; i < board.rows; i++) {
+        for (let j = 0; j < board.columns; j++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            if (i === activeRow) {
+                cell.classList.add('active');
+            }
+            const cellContent = document.createElement('div');
+            cellContent.className = 'cell-content';
+            cellContent.textContent = board.cells[i][j];
+            cell.appendChild(cellContent);
+
+            if (i === activeRow && board.cells[i][j] != null) {
+                cell.onclick = () => handleCellClick(i, j);
+            }
+            boardElement.appendChild(cell);
+        }
+    }
+}
+
+function handleCellClick(row, column) {
+    handleHumanMove(row, column);
+}
 
 function handleHumanMove(row, column) {
-  if (humanPlayer.play(board, { row, column })) {
-    updateBoard();
-    if (!board.hasAvailableMoves(column, row)) {
-      endGame();
-      return;
+    if (row !== activeRow) {
+        console.log("Nem a megengedett sorból választottál. Próbáld újra!");
+        return;
     }
-    handleComputerMove(column);
-  } else {
-    console.log("Nem sikerült lépni. Próbáld újra!");
-  }
+    if (humanPlayer.play(board, { row, column })) {
+        activeRow = column; // Frissíti az aktív sort a kiválasztott oszlop alapján
+        updateBoard();
+        if (!board.hasAvailableMoves(column, row)) {
+            endGame();
+            return;
+        }
+        handleComputerMove(column);
+    } else {
+        console.log("Nem sikerült lépni. Próbáld újra!");
+    }
 }
 
 function handleComputerMove(column) {
-  let availableRows = [];
-  for (let i = 0; i < board.rows; i++) {
-    if (board.cells[i][column] != null) {
-      availableRows.push(i);
+    let availableRows = [];
+    for (let i = 0; i < board.rows; i++) {
+        if (board.cells[i][column] != null) {
+            availableRows.push(i);
+        }
     }
-  }
 
-  if (availableRows.length === 0) {
-    endGame();
-    return;
-  }
+    if (availableRows.length === 0) {
+        endGame();
+        return;
+    }
 
-  let randomRowIndex = availableRows[Math.floor(Math.random() * availableRows.length)];
-  computerPlayer.play(board, { row: randomRowIndex, column });
-  updateBoard();
+    let randomRowIndex = availableRows[Math.floor(Math.random() * availableRows.length)];
+    computerPlayer.play(board, { row: randomRowIndex, column });
+    activeRow = randomRowIndex; // Frissíti az aktív sort a számítógép választása alapján
+    updateBoard();
 }
 
 function updateBoard() {
-  createBoard(board);
+    createBoard(board);
 }
 
 function endGame() {
-  if (humanPlayer.score > computerPlayer.score) {
-    console.log("Az emberi játékos nyert!");
-  } else if (humanPlayer.score < computerPlayer.score) {
-    console.log("A számítógép nyert!");
-  } else {
-    console.log("Döntetlen!");
-  }
+    if (humanPlayer.score > computerPlayer.score) {
+        console.log("Az emberi játékos nyert!");
+    } else if (humanPlayer.score < computerPlayer.score) {
+        console.log("A számítógép nyert!");
+    } else {
+        console.log("Döntetlen!");
+    }
 }
 
-// Ez az inicializáló kód az oldal betöltésekor fut le.
+// Inicializálja a táblát a kezdő állapotban.
 document.addEventListener('DOMContentLoaded', (event) => {
-  createBoard(board);
+    createBoard(board);
 });
