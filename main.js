59c06aff9b7c940599b07a4daec1c59729e7075b
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2023-12-08 20:46:11 +0100

    firebase import

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+import firebase from './firebase.js';
+
 function startGame() {
   // A játék kezdete
+
+  // A játékos nevét elmentjük a Firebase adatbázisba
+  const playerName = "John Doe";
+
+  firebase.database().ref("players").push({
+    name: playerName
+  });
 }
 
 function endGame() {
