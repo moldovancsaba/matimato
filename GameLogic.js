58baffdc8e20aa4277a0b578669a16705e6dbf77
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2023-12-10 22:31:40 +0100

    who is the winner

--- GameLogic.js
+++ GameLogic.js
@@ -94,33 +94,84 @@ function displayWinMessage() {
     const boardElement = document.getElementById('board');
     boardElement.innerHTML = ''; // Tábla törlése
 
-    // Felső üzenet
-    const messageRowTop = document.createElement('div');
-    messageRowTop.className = 'message-row';
-    messageRowTop.textContent = winMessageTop;
-    boardElement.appendChild(messageRowTop);
-
-    // Alsó üzenet
-    const messageRowBottom = document.createElement('div');
-    messageRowBottom.className = 'message-row';
-    messageRowBottom.textContent = winMessageBottom;
-    boardElement.appendChild(messageRowBottom);
-
-    // Új játék indítása 5 másodperc múlva
-    setTimeout(() => {
-        startNewGame();
-    }, 5000);
-}
-
-function startNewGame() {
-    // Létrehoz egy új táblát és inicializálja a játékot
-    board = new Board();
-    createBoard(board);
-    resetGame();
-}
-
-function resetGame() {
-    humanPlayer.score = 0;
-    computerPlayer.score = 0;
-    activeRow = 5; // Kezdő sor visszaállítása
-    displayMessage
+    function displayMessage(message) {
+        const messageElement = document.getElementById('message');
+        messageElement.textContent = message;
+    }
+    
+    function handleCellClick(row, column) {
+        displayMessage(`Cell clicked at row ${row}, column ${column}`);
+        handleHumanMove(row, column);
+    }
+    
+    function handleHumanMove(row, column) {
+        if (row !== activeRow) {
+            displayMessage("Nem a megengedett sorból választottál. Próbáld újra!");
+            return;
+        }
+        if (humanPlayer.play(board, { row, column })) {
+            activeRow = getNextActiveRow(column);
+            updateBoard();
+            if (!board.hasAvailableMoves(column, row)) {
+                endGame('human');
+                return;
+            }
+            handleComputerMove(column);
+        } else {
+            displayMessage("Nem sikerült lépni. Próbáld újra!");
+        }
+    }
+    
+    function handleComputerMove(column) {
+        let availableRows = [];
+        for (let i = 0; i < board.rows; i++) {
+            if (board.cells[i][column] != null) {
+                availableRows.push(i);
+            }
+        }
+    
+        if (availableRows.length === 0) {
+            endGame('computer');
+            return;
+        }
+    
+        let randomRowIndex = availableRows[Math.floor(Math.random() * availableRows.length)];
+        computerPlayer.play(board, { row: randomRowIndex, column });
+        activeRow = getNextActiveRow(randomRowIndex);
+        updateBoard();
+        if (!board.hasAvailableMoves(randomRowIndex, column)) {
+            endGame('computer');
+            return;
+        }
+    }
+    
+    function updateBoard() {
+        createBoard(board);
+    }
+    
+    function endGame(lastPlayer) {
+        let resultMessage = "";
+        if (humanPlayer.score > computerPlayer.score) {
+            resultMessage = "Az emberi játékos nyert!";
+            animateBoardForWin();
+        } else if (humanPlayer.score < computerPlayer.score) {
+            resultMessage = "A számítógép nyert!";
+        } else {
+            // Pontegyenlőség esetén az utoljára lépő játékos nyer
+            resultMessage = lastPlayer === 'human' ? "Az emberi játékos nyert!" : "A számítógép nyert!";
+        }
+        displayMessage(resultMessage);
+        setTimeout(startNewGame, 5000);
+    }
+    
+    function getNextActiveRow(currentColumn) {
+        // Következő aktív sor logikája
+        return currentColumn;
+    }
+    
+    // További funkciók: animateBoardForWin, startNewGame, stb.
+    
+    let activeRow = 4; // Kezdeti beállítás a középső sorra
+    
+    // Játék indítása
+    startNewGame();
