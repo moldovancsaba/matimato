987c988813130c3c9439185b3820cc24550e15d5
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2023-12-10 22:23:10 +0100

    winner

--- GameLogic.js
+++ GameLogic.js
@@ -53,13 +53,15 @@ function endGame() {
     let resultMessage = "";
     if (humanPlayer.score > computerPlayer.score) {
         resultMessage = "Az emberi játékos nyert!";
+        displayMessage(resultMessage);
+        animateBoardForWin();
     } else if (humanPlayer.score < computerPlayer.score) {
         resultMessage = "A számítógép nyert!";
+        displayMessage(resultMessage);
     } else {
         resultMessage = "Döntetlen!";
+        displayMessage(resultMessage);
     }
-    displayMessage(resultMessage);
-    // saveGameStats(); // Ha Firebase integrációt használsz, és implementálva van
 }
 
 function getNextActiveRow(currentColumn) {
@@ -67,7 +69,58 @@ function getNextActiveRow(currentColumn) {
     return currentColumn; // Ideiglenes megoldás
 }
 
-let activeRow = 4; // Kezdeti beállítás a középső sorra
+function animateBoardForWin() {
+    // Cellák eltüntetése és üzenet megjelenítése
+    for (let i = 0; i < board.rows; i++) {
+        for (let j = 0; j < board.columns; j++) {
+            setTimeout(() => {
+                const cell = document.querySelector(`.cell[row="${i}"][column="${j}"]`);
+                if (cell) {
+                    cell.style.visibility = 'hidden';
+                }
+            }, 100 * (i * board.columns + j));
+        }
+    }
 
-// A tábla létrehozásának inicializálása
-createBoard(board);
+    // Üzenet megjelenítése a táblán
+    setTimeout(() => {
+        displayWinMessage();
+    }, board.rows * board.columns * 100 + 1000);
+}
+
+function displayWinMessage() {
+    const winMessageTop = "WE HAVE A";
+    const winMessageBottom = " WINNER! ";
+    const boardElement = document.getElementById('board');
+    boardElement.innerHTML = ''; // Tábla törlése
+
+    // Felső üzenet
+    const messageRowTop = document.createElement('div');
+    messageRowTop.className = 'message-row';
+    messageRowTop.textContent = winMessageTop;
+    boardElement.appendChild(messageRowTop);
+
+    // Alsó üzenet
+    const messageRowBottom = document.createElement('div');
+    messageRowBottom.className = 'message-row';
+    messageRowBottom.textContent = winMessageBottom;
+    boardElement.appendChild(messageRowBottom);
+
+    // Új játék indítása 5 másodperc múlva
+    setTimeout(() => {
+        startNewGame();
+    }, 5000);
+}
+
+function startNewGame() {
+    // Létrehoz egy új táblát és inicializálja a játékot
+    board = new Board();
+    createBoard(board);
+    resetGame();
+}
+
+function resetGame() {
+    humanPlayer.score = 0;
+    computerPlayer.score = 0;
+    activeRow = 5; // Kezdő sor visszaállítása
+    displayMessage
