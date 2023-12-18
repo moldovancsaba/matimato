e913c9c7392213aec7704c760f5bab4d86d83be8
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2023-12-18 15:23:49 +0100

    yo

--- Core.js
+++ Core.js
@@ -1,4 +1,3 @@
-// Játék tábla osztály
 class Board {
     constructor() {
         this.rows = 5;
@@ -6,38 +5,63 @@ class Board {
         this.cells = this.createInitialBoard();
     }
 
-    // Random számok generálása a táblára
     createInitialBoard() {
         let board = [];
         for (let i = 0; i < this.rows; i++) {
             let row = [];
             for (let j = 0; j < this.columns; j++) {
-                let num = Math.floor(Math.random() * 9) + 1;
-                row.push(num);
+                row.push(Math.floor(Math.random() * 9) + 1);
             }
             board.push(row);
         }
         return board;
     }
 
-    // Szám eltávolítása a tábláról
     removeNumber(row, column) {
         this.cells[row][column] = 0;
     }
 }
 
-// A tábla létrehozása és megjelenítése
-function createBoard(board) {
+class Player {
+    constructor() {
+        this.score = 0;
+    }
+
+    play(board, position) {
+        if (board.cells[position.row][position.column] !== 0) {
+            this.score += board.cells[position.row][position.column];
+            board.removeNumber(position.row, position.column);
+            return true;
+        }
+        return false;
+    }
+}
+
+const board = new Board();
+const humanPlayer = new Player();
+
+function displayScore() {
+    const scoreElement = document.getElementById('score');
+    scoreElement.textContent = `Score: ${humanPlayer.score}`;
+}
+
+function handleCellClick(row, column) {
+    if (humanPlayer.play(board, { row, column })) {
+        updateBoard();
+        displayScore();
+    }
+}
+
+function updateBoard() {
     const boardElement = document.getElementById('board');
     boardElement.innerHTML = '';
-
     for (let i = 0; i < board.rows; i++) {
         for (let j = 0; j < board.columns; j++) {
             const cell = document.createElement('div');
             cell.className = 'cell';
             const cellContent = document.createElement('div');
             cellContent.className = 'cell-content';
-            cellContent.textContent = board.cells[i][j];
+            cellContent.textContent = board.cells[i][j] === 0 ? '' : board.cells[i][j];
             cell.appendChild(cellContent);
             cell.onclick = () => handleCellClick(i, j);
             boardElement.appendChild(cell);
@@ -45,56 +69,5 @@ function createBoard(board) {
     }
 }
 
-// Globális változók
-const board = new Board();
-let totalScore = 0;
-
-// Kattintás kezelése
-function handleCellClick(row, column) {
-    if (board.cells[row][column] !== 0) {
-        totalScore += board.cells[row][column];
-        board.removeNumber(row, column);
-        updateBoard();
-        displayScore();
-    }
-}
-
-// Tábla frissítése
-function updateBoard() {
-    createBoard(board);
-}
-
-// Pontszám megjelenítése
-function displayScore() {
-    const scoreElement = document.getElementById('score');
-    scoreElement.textContent = "Total Score: " + totalScore;
-}
-
-// Tábla és pontszám inicializálása
-createBoard(board);
-displayScore();
-// Stílus és HTML elemek inicializálása
-document.addEventListener('DOMContentLoaded', function() {
-    // Fejléc stílusának beállítása
-    const header = document.createElement('h1');
-    header.textContent = 'Matimato Game';
-    header.style.textAlign = 'center';
-    header.style.backgroundColor = '#3399aa';
-    header.style.color = 'white';
-    header.style.padding = '10px';
-    header.style.margin = '0';
-    header.style.position = 'fixed';
-    header.style.top = '0';
-    header.style.width = '100%';
-    header.style.zIndex = '1000';
-
-    // Pontszám stílusának beállítása
-    const scoreElement = document.createElement('div');
-    scoreElement.id = 'score';
-    scoreElement.style.textAlign = 'center';
-    scoreElement.style.marginTop = '60px';
-
-    // Elemek hozzáadása a dokumentumhoz
-    document.body.insertBefore(header, document.body.firstChild);
-    document.body.insertBefore(scoreElement, document.getElementById('board'));
-});
+// A játék inicializálása
+updateBoard();
