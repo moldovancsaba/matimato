b4efd4578b99ac427d794e9356bf326d815849a4
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2023-12-27 15:33:16 +0100

    1382

--- matimato-core.js
+++ matimato-core.js
@@ -95,6 +95,7 @@ function handleCellClick(row, column) {
         board.cells[row][column] = '•';
         highlightCell(row, column);
         isPlayerTurn = false;
+        lastSelectedRow = row; // Frissíti az utoljára választott sort
         setTimeout(computerMove, 500);
     }
     updateScoreDisplay();
@@ -103,10 +104,10 @@ function handleCellClick(row, column) {
 
 function computerMove() {
     if (!isPlayerTurn) {
-        let availableCells = getAvailableCells();
+        let availableCells = getAvailableCellsInRow(lastSelectedRow); // Csak az utoljára választott sorból választhat
 
         if (availableCells.length > 0) {
-            // A legnagyobb számot tartalmazó cella kiválasztása
+            // A legnagyobb számot tartalmazó cella kiválasztása az adott sorban
             let maxCell = availableCells.reduce((max, cell) => board.cells[cell.row][cell.column] > board.cells[max.row][max.column] ? cell : max, availableCells[0]);
             aiScore += board.cells[maxCell.row][maxCell.column];
             board.cells[maxCell.row][maxCell.column] = '•';
@@ -119,13 +120,11 @@ function computerMove() {
     }
 }
 
-function getAvailableCells() {
+function getAvailableCellsInRow(row) {
     let availableCells = [];
-    for (let i = 0; i < board.rows; i++) {
-        for (let j = 0; j < board.columns; j++) {
-            if (board.cells[i][j] !== '•') {
-                availableCells.push({ row: i, column: j });
-            }
+    for (let j = 0; j < board.columns; j++) {
+        if (board.cells[row][j] !== '•') {
+            availableCells.push({ row: row, column: j });
         }
     }
     return availableCells;
@@ -138,7 +137,7 @@ function checkEndGame() {
 }
 
 function canComputerMove() {
-    return getAvailableCells().length > 0;
+    return getAvailableCellsInRow(lastSelectedRow).length > 0;
 }
 
 function canPlayerMove() {
