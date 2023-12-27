e4d96d0f58439fcc9cb323a4b784dab022d750f8
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2023-12-27 14:17:40 +0100

    1366

--- matimato-core.js
+++ matimato-core.js
@@ -80,6 +80,7 @@ function createBoard() {
 
 
 
+
 //------------------------------
 // #MM0003 Game Logic
 //------------------------------
@@ -88,11 +89,11 @@ let lastSelectedRow = null; // Utoljára választott sor
 let lastSelectedColumn = null; // Utoljára választott oszlop
 
 function handleCellClick(row, column) {
-    if (isPlayerTurn && board.cells[row][column] !== '•') {
+    if (isPlayerTurn && board.cells[row][column] !== '•' && (lastSelectedColumn === null || lastSelectedColumn === column)) {
         playerScore += board.cells[row][column];
         board.cells[row][column] = '•';
         highlightCell(row, column);
-        highlightRow(row); // Kiemeli a sort
+        highlightColumn(column); // Kiemeli az oszlopot
         isPlayerTurn = false;
         lastSelectedColumn = column; // Frissíti az utoljára választott oszlopot
         setTimeout(computerMove, 500);
@@ -103,15 +104,16 @@ function handleCellClick(row, column) {
 
 function computerMove() {
     if (!isPlayerTurn) {
-        let availableCells = getAvailableCells();
+        let availableCells = getAvailableCellsInRow(lastSelectedRow);
 
         if (availableCells.length > 0) {
             let maxCell = availableCells.reduce((max, cell) => board.cells[cell.row][cell.column] > board.cells[max.row][max.column] ? cell : max, availableCells[0]);
             aiScore += board.cells[maxCell.row][maxCell.column];
             board.cells[maxCell.row][maxCell.column] = '•';
             highlightCell(maxCell.row, maxCell.column);
-            highlightColumn(maxCell.column); // Kiemeli az oszlopot
+            highlightRow(maxCell.row); // Kiemeli a sort
             isPlayerTurn = true;
+            lastSelectedRow = maxCell.row; // Frissíti az utoljára választott sort
             updateScoreDisplay();
         } else {
             checkEndGame();
@@ -119,13 +121,11 @@ function computerMove() {
     }
 }
 
-function getAvailableCells() {
+function getAvailableCellsInRow(row) {
     let availableCells = [];
-    for (let i = 0; i < board.rows; i++) {
-        for (let j = 0; j < board.columns; j++) {
-            if (board.cells[i][j] !== '•') {
-                availableCells.push({ row: i, column: j });
-            }
+    for (let j = 0; j < board.columns; j++) {
+        if (board.cells[row][j] !== '•') {
+            availableCells.push({ row: row, column: j });
         }
     }
     return availableCells;
@@ -138,11 +138,21 @@ function checkEndGame() {
 }
 
 function canComputerMove() {
-    return getAvailableCells().length > 0;
+    return getAvailableCellsInRow(lastSelectedRow).length > 0;
 }
 
 function canPlayerMove() {
-    return getAvailableCells().length > 0;
+    return lastSelectedColumn === null ? true : getAvailableCellsInColumn(lastSelectedColumn).length > 0;
+}
+
+function getAvailableCellsInColumn(column) {
+    let availableCells = [];
+    for (let i = 0; i < board.rows; i++) {
+        if (board.cells[i][column] !== '•') {
+            availableCells.push({ row: i, column: column });
+        }
+    }
+    return availableCells;
 }
 
 function endGame() {
@@ -159,6 +169,8 @@ function endGame() {
     document.getElementById('end-game-message').style.display = 'block';
     document.getElementById('winner-message').textContent = winner;
 }
+
+
 
 
 
