e3d0af99b4a864d1fa135385bdaeb22593c11732
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2023-12-30 14:56:04 +0100

    1482

--- matimato-basic.js
+++ matimato-basic.js
@@ -85,7 +85,6 @@ function createBoard() {
 
 
 
-
 //--------------------------------------------------------------------
 // #MM0003 Game Logic ------------------------------------------------
 //--------------------------------------------------------------------
@@ -93,37 +92,40 @@ function createBoard() {
 let lastSelectedRow = null; // Last selected row
 let lastSelectedColumn = null; // Last selected column
 
-function handleCellClick(row, column) {
-    if (isPlayerTurn && board.cells[row][column] !== '•' && (lastSelectedRow === null || lastSelectedRow === row)) {
-        playerScore += board.cells[row][column];
-        board.cells[row][column] = '•';
+function handleCellClick(row, column, masterBoard) {
+    if (isPlayerTurn && masterBoard.cells[row][column] !== '•' && (lastSelectedColumn === null || lastSelectedColumn === column)) {
+        playerScore += masterBoard.cells[row][column];
+        masterBoard.cells[row][column] = '•';
         highlightCell(row, column);
         highlightColumn(column); // Highlight the column where the player moved
         isPlayerTurn = false;
         lastSelectedColumn = column; // Update the last selected column
-        setTimeout(computerMove, 500);
+        setTimeout(masterComputerMove, 500);
     }
     updateScoreDisplay();
 }
 
-function computerMove() {
+function masterComputerMove() {
     if (!isPlayerTurn) {
-        let availableCells = getAvailableCellsInColumn(lastSelectedColumn); // Choose only from the last selected column
-
-        if (availableCells.length > 0) {
-            let maxCell = availableCells.reduce((max, cell) => board.cells[cell.row][cell.column] > board.cells[max.row][max.column] ? cell : max, availableCells[0]);
-            aiScore += board.cells[maxCell.row][maxCell.column];
-            board.cells[maxCell.row][maxCell.column] = '•';
-            highlightCell(maxCell.row, maxCell.column);
-            highlightRow(maxCell.row); // Highlight the row where the AI moved
+        let bestMove = calculateBestMove(masterBoard);
+        if (bestMove) {
+            aiScore += masterBoard.cells[bestMove.row][bestMove.column];
+            masterBoard.cells[bestMove.row][bestMove.column] = '•';
+            highlightCell(bestMove.row, bestMove.column);
+            highlightRow(bestMove.row); // Highlight the row where the AI moved
             isPlayerTurn = true;
-            lastSelectedRow = maxCell.row; // Update the last selected row
+            lastSelectedRow = bestMove.row; // Update the last selected row
             updateScoreDisplay();
             checkPlayerMovePossibility(); // Check if the player can move
         } else {
             checkEndGame();
         }
     }
+}
+
+function calculateBestMove(board) {
+    // Implement AI logic to calculate the best move
+    // Ensure it considers the last selected row by the player
 }
 
 function checkPlayerMovePossibility() {
@@ -134,8 +136,8 @@ function checkPlayerMovePossibility() {
 
 function getAvailableCellsInColumn(column) {
     let availableCells = [];
-    for (let i = 0; i < board.rows; i++) {
-        if (board.cells[i][column] !== '•') {
+    for (let i = 0; i < masterBoard.size; i++) {
+        if (masterBoard.cells[i][column] !== '•') {
             availableCells.push({ row: i, column: column });
         }
     }
@@ -143,55 +145,29 @@ function getAvailableCellsInColumn(column) {
 }
 
 function checkEndGame() {
-    if ((!isPlayerTurn && !canComputerMove()) || (isPlayerTurn && !canPlayerMove())) {
-        endGame();
-    }
+    // Implement logic to check if the game has ended
 }
 
 function endGame() {
-    let winner;
-    if (playerScore > aiScore) {
-        winner = 'You win!';
-    } else if (aiScore > playerScore) {
-        winner = 'AI wins!';
-    } else {
-        winner = 'Draw!';
-    }
-
-    document.getElementById('board').style.display = 'none';
-    document.getElementById('end-game-message').style.display = 'block';
-    document.getElementById('winner-message').textContent = winner;
-}
-
-function canComputerMove() {
-    return getAvailableCellsInColumn(lastSelectedColumn).length > 0;
+    // Implement logic to handle the end of the game
 }
 
 function canPlayerMove() {
-    return getAvailableCellsInRow(lastSelectedRow).length > 0;
+    // Implement logic to check if the player can make a move
+    // Consider only the last selected column by the AI
 }
 
 function getAvailableCellsInRow(row) {
     let availableCells = [];
-    for (let j = 0; j < board.columns; j++) {
-        if (board.cells[row][j] !== '•') {
+    for (let j = 0; j < masterBoard.size; j++) {
+        if (masterBoard.cells[row][j] !== '•') {
             availableCells.push({ row: row, column: j });
         }
     }
     return availableCells;
 }
 
-function getAvailableCells() {
-    let availableCells = [];
-    for (let i = 0; i < board.rows; i++) {
-        for (let j = 0; j < board.columns; j++) {
-            if (board.cells[i][j] !== '•') {
-                availableCells.push({ row: i, column: j });
-            }
-        }
-    }
-    return availableCells;
-}
+// Other necessary functions
 
 
 
