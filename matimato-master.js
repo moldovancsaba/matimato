cc3b8325494c16b8eef5329bd3de1137ba2af7bb
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2023-12-30 14:19:48 +0100

    master

---
+++ matimato-master.js
@@ -0,0 +1,287 @@
+//------------------------------
+// matimato-master.js ----------
+//------------------------------
+// JAVASCRIPT STARTS HERE ------
+//------------------------------
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------
+// #MM0001 Global variables ------------------------------------------
+//--------------------------------------------------------------------
+
+class MasterBoard {
+    constructor(size) {
+        this.size = size; // Board size, always 7x7 for Master level
+        this.cells = this.createInitialBoard();
+    }
+
+    createInitialBoard() {
+        let board = [];
+        for (let i = 0; i < this.size; i++) {
+            let row = [];
+            for (let j = 0; j < this.size; j++) {
+                row.push(Math.floor(Math.random() * 9) + 1);
+            }
+            board.push(row);
+        }
+        return board;
+    }
+}
+
+// Initializing the Master game board with 7x7 size
+let masterBoard = new MasterBoard(7);
+let playerScore = 0; // Player's score
+let aiScore = 0;     // AI's score
+let isPlayerTurn = true; // Flag to check if it's player's turn
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------
+// #MM0002 Create Gamefield ------------------------------------------
+//--------------------------------------------------------------------
+
+function createMasterBoard() {
+    const boardElement = document.getElementById('board');
+    boardElement.innerHTML = '';
+    boardElement.style.gridTemplateRows = `repeat(${masterBoard.size}, 1fr)`;
+    boardElement.style.gridTemplateColumns = `repeat(${masterBoard.size}, 1fr)`;
+
+    for (let i = 0; i < masterBoard.size; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.className = 'row';
+        for (let j = 0; j < masterBoard.size; j++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.textContent = masterBoard.cells[i][j];
+            cell.setAttribute('row', i);
+            cell.setAttribute('column', j);
+            cell.addEventListener('click', () => handleCellClick(i, j, masterBoard));
+            rowDiv.appendChild(cell);
+        }
+        boardElement.appendChild(rowDiv);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------
+// #MM0003 Game Logic ------------------------------------------------
+//--------------------------------------------------------------------
+
+function handleCellClick(row, column, masterBoard) {
+    if (isPlayerTurn && masterBoard.cells[row][column] !== '•') {
+        playerScore += masterBoard.cells[row][column];
+        masterBoard.cells[row][column] = '•';
+        highlightCell(row, column);
+        isPlayerTurn = false;
+        setTimeout(masterComputerMove, 500);
+    }
+    updateScoreDisplay();
+}
+
+function masterComputerMove() {
+    if (!isPlayerTurn) {
+        let bestMove = calculateBestMove(masterBoard);
+        if (bestMove) {
+            aiScore += masterBoard.cells[bestMove.row][bestMove.column];
+            masterBoard.cells[bestMove.row][bestMove.column] = '•';
+            highlightCell(bestMove.row, bestMove.column);
+            isPlayerTurn = true;
+            updateScoreDisplay();
+        } else {
+            checkEndGame();
+        }
+    }
+}
+
+function calculateBestMove(board) {
+    let bestScoreDiff = -Infinity;
+    let bestMove = null;
+
+    // Iterate through all available cells and calculate the best move
+    for (let i = 0; i < board.size; i++) {
+        for (let j = 0; j < board.size; j++) {
+            if (board.cells[i][j] !== '•') {
+                let tempScore = board.cells[i][j];
+                board.cells[i][j] = '•';
+                let playerBestMove = findPlayerBestMove(board);
+                let scoreDiff = tempScore - playerBestMove;
+                if (scoreDiff > bestScoreDiff) {
+                    bestScoreDiff = scoreDiff;
+                    bestMove = { row: i, column: j };
+                }
+                board.cells[i][j] = tempScore; // Reset the cell value
+            }
+        }
+    }
+
+    return bestMove;
+}
+
+function findPlayerBestMove(board) {
+    let bestScore = 0;
+    for (let i = 0; i < board.size; i++) {
+        for (let j = 0; j < board.size; j++) {
+            if (board.cells[i][j] !== '•') {
+                bestScore = Math.max(bestScore, board.cells[i][j]);
+            }
+        }
+    }
+    return bestScore;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------
+// #MM0004 UI Functions ----------------------------------------------
+//--------------------------------------------------------------------
+
+function highlightCell(row, column) {
+    let cellElement = document.querySelector(`.cell[row="${row}"][column="${column}"]`);
+    if (cellElement) {
+        cellElement.style.backgroundColor = '#444444';
+        setTimeout(() => {
+            cellElement.textContent = '•';
+            cellElement.style.backgroundColor = '#444444';
+        }, 500);
+    }
+}
+
+function clearHighlights() {
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.style.border = '';
+        cell.style.boxSizing = '';
+    });
+}
+
+function updateScoreDisplay() {
+    const playerScoreElement = document.getElementById('player-score');
+    const aiScoreElement = document.getElementById('ai-score');
+    playerScoreElement.textContent = `You: ${playerScore}`;
+    aiScoreElement.textContent = `AI: ${aiScore}`;
+}
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------
+// #MM0005 Initialize Game and Firebase Logic ------------------------
+//--------------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', () => {
+    startMasterGame(); // Start the Master mode game immediately when the page loads
+});
+
+function startMasterGame() {
+    masterResetGame();
+    document.getElementById('board').style.display = 'grid';
+    document.getElementById('score').style.display = 'block';
+    document.getElementById('start-screen').style.display = 'none';
+}
+
+function masterResetGame() {
+    board = new Board(7, 7); // Create a new 7x7 board for Master mode
+    board.cells = board.createInitialBoard(); // Generate new cell values for the board
+    playerScore = 0;
+    aiScore = 0;
+    isPlayerTurn = true;
+    lastSelectedRow = null;
+    lastSelectedColumn = null;
+    createBoard();
+    updateScoreDisplay();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------
+// #MM0006 Start & End -----------------------------------------------
+//--------------------------------------------------------------------
+
+function hideGame() {
+    document.getElementById('board').style.display = 'none';
+    document.getElementById('score').style.display = 'none';
+    document.getElementById('end-game-message').style.display = 'none';
+    document.getElementById('start-screen').style.display = 'block';
+}
+
+// Event handler for the Restart button
+document.getElementById('restart-button').addEventListener('click', () => {
+    masterResetGame();
+    startMasterGame();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------
+// END OF CODE -----------------
+//------------------------------
+// CREATED BY MOLDOVAN ---------
+//------------------------------
+// JAVASCRIPT WRITTEN BY GPT ---
+//------------------------------
